5d97e485e69b389f45f26e15a921e87a8fbb08c8
yago-codev <yagomilano92@gmail.com>
2020-03-20 02:39:22 -0300

    Criando validações com Mongoose

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -8,7 +8,7 @@ const creditSchema = new mongoose.Schema({
 
 const debitSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: { type: Number, min: 0, required: [ true, 'Por favor, informe o valor do débito!' ] },
   status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 });
 
@@ -16,8 +16,8 @@ const billingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   month: { type: Number, min: 1, max: 12, required: true },
   year: { type: Number, min: 1900, max: 2100, required: true },
-  credits: [ creditSchema ],
-  debits: [ debitSchema ]
+  credits: [creditSchema],
+  debits: [debitSchema]
 });
 
 module.exports = restful.model('BillingCycle', billingCycleSchema);
--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // atribuindo  a API de Promises do Node na parte de Promise do Mongo
 
 module.exports = mongoose.connect('mongodb://localhost/mymoney');
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é meior que o limite máximo de '{MAX}."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
